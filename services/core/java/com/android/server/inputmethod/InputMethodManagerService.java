511e65bcfeac76ae508ab46a24e61928070db053
PortalRomOS <portalromOS@gmail.com>
2022-10-20 14:25:38 +0000

    change to portalrom

--- services/core/java/com/android/server/inputmethod/InputMethodManagerService.java
+++ services/core/java/com/android/server/inputmethod/InputMethodManagerService.java
@@ -192,8 +192,8 @@ import com.android.server.statusbar.StatusBarManagerService;
 import com.android.server.utils.PriorityDump;
 import com.android.server.wm.WindowManagerInternal;
 
-import lineageos.hardware.LineageHardwareManager;
-import lineageos.providers.LineageSettings;
+import portalrom.hardware.PortalRomHardwareManager;
+import portalrom.providers.PortalRomSettings;
 
 import java.io.FileDescriptor;
 import java.io.IOException;
@@ -368,7 +368,7 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
     private boolean mShowOngoingImeSwitcherForPhones;
     private boolean mNotificationShown;
 
-    private LineageHardwareManager mLineageHardware;
+    private PortalRomHardwareManager mPortalRomHardware;
 
     static class SessionState {
         final ClientState client;
@@ -1071,21 +1071,21 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
                     Settings.Secure.SHOW_IME_WITH_HARD_KEYBOARD), false, this, userId);
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.ACCESSIBILITY_SOFT_KEYBOARD_MODE), false, this, userId);
-            if (mLineageHardware.isSupported(
-                    LineageHardwareManager.FEATURE_HIGH_TOUCH_POLLING_RATE)) {
-                resolver.registerContentObserver(LineageSettings.System.getUriFor(
-                        LineageSettings.System.HIGH_TOUCH_POLLING_RATE_ENABLE),
+            if (mPortalRomHardware.isSupported(
+                    PortalRomHardwareManager.FEATURE_HIGH_TOUCH_POLLING_RATE)) {
+                resolver.registerContentObserver(PortalRomSettings.System.getUriFor(
+                        PortalRomSettings.System.HIGH_TOUCH_POLLING_RATE_ENABLE),
                         false, this, userId);
             }
-            if (mLineageHardware.isSupported(
-                    LineageHardwareManager.FEATURE_HIGH_TOUCH_SENSITIVITY)) {
-                resolver.registerContentObserver(LineageSettings.System.getUriFor(
-                        LineageSettings.System.HIGH_TOUCH_SENSITIVITY_ENABLE),
+            if (mPortalRomHardware.isSupported(
+                    PortalRomHardwareManager.FEATURE_HIGH_TOUCH_SENSITIVITY)) {
+                resolver.registerContentObserver(PortalRomSettings.System.getUriFor(
+                        PortalRomSettings.System.HIGH_TOUCH_SENSITIVITY_ENABLE),
                         false, this, userId);
             }
-            if (mLineageHardware.isSupported(LineageHardwareManager.FEATURE_TOUCH_HOVERING)) {
-                resolver.registerContentObserver(LineageSettings.Secure.getUriFor(
-                        LineageSettings.Secure.FEATURE_TOUCH_HOVERING), false, this, userId);
+            if (mPortalRomHardware.isSupported(PortalRomHardwareManager.FEATURE_TOUCH_HOVERING)) {
+                resolver.registerContentObserver(PortalRomSettings.Secure.getUriFor(
+                        PortalRomSettings.Secure.FEATURE_TOUCH_HOVERING), false, this, userId);
             }
             mRegistered = true;
         }
@@ -1095,12 +1095,12 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
                     Settings.Secure.SHOW_IME_WITH_HARD_KEYBOARD);
             final Uri accessibilityRequestingNoImeUri = Settings.Secure.getUriFor(
                     Settings.Secure.ACCESSIBILITY_SOFT_KEYBOARD_MODE);
-            final Uri highTouchPollingRateUri = LineageSettings.System.getUriFor(
-                    LineageSettings.System.HIGH_TOUCH_POLLING_RATE_ENABLE);
-            final Uri touchSensitivityUri = LineageSettings.System.getUriFor(
-                    LineageSettings.System.HIGH_TOUCH_SENSITIVITY_ENABLE);
-            final Uri touchHoveringUri = LineageSettings.Secure.getUriFor(
-                    LineageSettings.Secure.FEATURE_TOUCH_HOVERING);
+            final Uri highTouchPollingRateUri = PortalRomSettings.System.getUriFor(
+                    PortalRomSettings.System.HIGH_TOUCH_POLLING_RATE_ENABLE);
+            final Uri touchSensitivityUri = PortalRomSettings.System.getUriFor(
+                    PortalRomSettings.System.HIGH_TOUCH_SENSITIVITY_ENABLE);
+            final Uri touchHoveringUri = PortalRomSettings.Secure.getUriFor(
+                    PortalRomSettings.Secure.FEATURE_TOUCH_HOVERING);
             synchronized (mMethodMap) {
                 if (showImeUri.equals(uri)) {
                     mMenuController.updateKeyboardFromSettingsLocked();
@@ -1809,7 +1809,7 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
                         !mUserManagerInternal.isUserUnlockingOrUnlocked(currentUserId));
 
                 // Must happen before registerContentObserverLocked
-                mLineageHardware = LineageHardwareManager.getInstance(mContext);
+                mPortalRomHardware = PortalRomHardwareManager.getInstance(mContext);
 
                 updateTouchHovering();
                 updateTouchPollingRate();
@@ -3025,30 +3025,30 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
     }
 
     private void updateTouchPollingRate() {
-        if (!mLineageHardware.isSupported(LineageHardwareManager.FEATURE_HIGH_TOUCH_POLLING_RATE)) {
+        if (!mPortalRomHardware.isSupported(PortalRomHardwareManager.FEATURE_HIGH_TOUCH_POLLING_RATE)) {
             return;
         }
-        final boolean enabled = LineageSettings.System.getInt(mContext.getContentResolver(),
-                LineageSettings.System.HIGH_TOUCH_POLLING_RATE_ENABLE, 0) == 1;
-        mLineageHardware.set(LineageHardwareManager.FEATURE_HIGH_TOUCH_POLLING_RATE, enabled);
+        final boolean enabled = PortalRomSettings.System.getInt(mContext.getContentResolver(),
+                PortalRomSettings.System.HIGH_TOUCH_POLLING_RATE_ENABLE, 0) == 1;
+        mPortalRomHardware.set(PortalRomHardwareManager.FEATURE_HIGH_TOUCH_POLLING_RATE, enabled);
     }
 
     private void updateTouchSensitivity() {
-        if (!mLineageHardware.isSupported(LineageHardwareManager.FEATURE_HIGH_TOUCH_SENSITIVITY)) {
+        if (!mPortalRomHardware.isSupported(PortalRomHardwareManager.FEATURE_HIGH_TOUCH_SENSITIVITY)) {
             return;
         }
-        final boolean enabled = LineageSettings.System.getInt(mContext.getContentResolver(),
-                LineageSettings.System.HIGH_TOUCH_SENSITIVITY_ENABLE, 0) == 1;
-        mLineageHardware.set(LineageHardwareManager.FEATURE_HIGH_TOUCH_SENSITIVITY, enabled);
+        final boolean enabled = PortalRomSettings.System.getInt(mContext.getContentResolver(),
+                PortalRomSettings.System.HIGH_TOUCH_SENSITIVITY_ENABLE, 0) == 1;
+        mPortalRomHardware.set(PortalRomHardwareManager.FEATURE_HIGH_TOUCH_SENSITIVITY, enabled);
     }
 
     private void updateTouchHovering() {
-        if (!mLineageHardware.isSupported(LineageHardwareManager.FEATURE_TOUCH_HOVERING)) {
+        if (!mPortalRomHardware.isSupported(PortalRomHardwareManager.FEATURE_TOUCH_HOVERING)) {
             return;
         }
-        final boolean enabled = LineageSettings.Secure.getInt(mContext.getContentResolver(),
-                LineageSettings.Secure.FEATURE_TOUCH_HOVERING, 0) == 1;
-        mLineageHardware.set(LineageHardwareManager.FEATURE_TOUCH_HOVERING, enabled);
+        final boolean enabled = PortalRomSettings.Secure.getInt(mContext.getContentResolver(),
+                PortalRomSettings.Secure.FEATURE_TOUCH_HOVERING, 0) == 1;
+        mPortalRomHardware.set(PortalRomHardwareManager.FEATURE_TOUCH_HOVERING, enabled);
     }
 
     /* package */ void setInputMethodLocked(String id, int subtypeId) {
